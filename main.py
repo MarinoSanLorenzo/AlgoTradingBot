eac7e9b953a52898578d7d42eb851e83080427d0
marino.sanlorenzo <marino.sanlorenzo@aion.eu>
2021-06-15 23:41:57 +0200

    1st commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from binance import Client, ThreadedDepthCacheManager, ThreadedWebsocketManager
+
+client = Client()
+
+depth = client.get_order_book(symbol='BNBBTC')
+
+# place a test market buy order, to place an actual order use the create_order function
+order = client.create_test_order(
+    symbol='BNBBTC',
+    side=Client.SIDE_BUY,
+    type=Client.ORDER_TYPE_MARKET,
+    quantity=100)
